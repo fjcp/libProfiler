13f302afe5614aeea5f55743ae8ef3342fdb021d
FranciscoCastillo <francisco.castillo@sener.es>
2020-06-09 09:55:02 +0200

    Manage end lines, fix error in regular expresion

--- test_parse_cpp_method.py
+++ test_parse_cpp_method.py
@@ -5,13 +5,18 @@ import glob
 import os
 
 def string_to_string_list(string):
-    return string.split("\n")
+    str_list_out =[]
+    str_list_aux = string.split("\n")
+    for string in str_list_aux:
+        string +='\n'
+        str_list_out.append(string)
+    return str_list_out
 
 def string_list_to_string(string_list):
-    return "\n".join(string_list)
+    return "".join(string_list)
 
 def add_cpp_include(str_list, include_name):
-    str_list.insert(0, '#include "' + include_name + '"')
+    str_list.insert(0, '#include "' + include_name + '"\n')
     return str_list
 
 
@@ -19,19 +24,22 @@ def add_cpp_macros_in_methods(file_lines, initial_macro, final_macro):
     file_lines_out = []
 
     method_definition = False
+    final_macro_pending = False
 
     for line in file_lines:
         file_lines_out.append(line)
 
-        if re.search(r'[a-zA-Z_][a-zA-Z0-9_]*::[a-zA-Z_][a-zA-Z0-9_]*(.*)', line):
+        if re.search(r'[a-zA-Z_][a-zA-Z0-9_]*::[a-zA-Z_][a-zA-Z0-9_]*\(.*', line):
             method_definition = True
 
-        if method_definition and line.endswith('{'):
-            file_lines_out.append(initial_macro + "();")
+        if method_definition and not final_macro_pending and line.endswith('{\n'):
+            file_lines_out.append(initial_macro + "();\n")
             method_definition = False
+            final_macro_pending = True
 
-        if line == "}":
-            file_lines_out.insert(len(file_lines_out) - 1, final_macro + "();")
+        if line == "}\n" and final_macro_pending:
+            file_lines_out.insert(len(file_lines_out) - 1, final_macro + "();\n")
+            final_macro_pending = False
 
     return file_lines_out
 
@@ -50,15 +58,14 @@ def add_cpp_include_and_macros(file_lines_in, include_name, initial_macro, final
 def test_empty_file():
     file_lines_in = []
     file_lines_out = add_cpp_include(file_lines_in, "MyInclude.h")
-    assert file_lines_out == ['#include "MyInclude.h"']
+    assert file_lines_out == ['#include "MyInclude.h"\n']
 
 
 def test_method_one_line():
     cpp_code_in = """\
 void A::B()
 {
-}
-"""
+}"""
     file_lines_in = string_to_string_list(cpp_code_in)
     file_lines_out = add_cpp_include_and_macros(
         file_lines_in, "MyInclude.h", "MACRO_INITIAL", "MACRO_FINAL"
@@ -79,8 +86,7 @@ def test_method_one_line_blank_lines_before():
 
 void A::B()
 {
-}
-"""
+}"""
     file_lines_in = string_to_string_list(cpp_code_in)
     file_lines_out = add_cpp_include_and_macros(
         file_lines_in, "MyInclude.h", "MACRO_INITIAL", "MACRO_FINAL"
@@ -100,8 +106,7 @@ MACRO_FINAL();
 def test_method_one_line_open_bracket_same_line():
     cpp_code_in = """\
 void A::B() {
-}
-"""
+}"""
     file_lines_in = string_to_string_list(cpp_code_in)
     file_lines_out = add_cpp_include_and_macros(
         file_lines_in, "MyInclude.h", "MACRO_INITIAL", "MACRO_FINAL"
@@ -118,8 +123,7 @@ MACRO_FINAL();
 
 def test_method_one_line_empty_body():
     cpp_code_in = """\
-void A::B() {}
-"""
+void A::B() {}"""
     file_lines_in = string_to_string_list(cpp_code_in)
     file_lines_out = add_cpp_include_and_macros(
         file_lines_in, "MyInclude.h", "MACRO_INITIAL", "MACRO_FINAL"
@@ -138,8 +142,7 @@ A::B(int foo,
      int bar)
 {
 
-}
-"""
+}"""
     file_lines_in = string_to_string_list(cpp_code_in)
     file_lines_out = add_cpp_include_and_macros(
         file_lines_in, "MyInclude.h", "MACRO_INITIAL", "MACRO_FINAL"
@@ -158,7 +161,118 @@ MACRO_FINAL();
 """
     assert string_list_to_string(file_lines_out) == cpp_code_out
 
+def test_method_destructor():
+    cpp_code_in = """\
+A::~A(){
+
+}"""
+    file_lines_in = string_to_string_list(cpp_code_in)
+    file_lines_out = add_cpp_include_and_macros(
+        file_lines_in, "MyInclude.h", "MACRO_INITIAL", "MACRO_FINAL"
+    )
+
+    cpp_code_out = """\
+#include "MyInclude.h"
+A::~A(){
+
+}
+"""
+    assert string_list_to_string(file_lines_out) == cpp_code_out
+
+def test_method_constructor_and_destructor():
+    cpp_code_in = """\
+A::A()
+{
+
+}
+A::~A(){
+
+}"""
+    file_lines_in = string_to_string_list(cpp_code_in)
+    file_lines_out = add_cpp_include_and_macros(
+        file_lines_in, "MyInclude.h", "MACRO_INITIAL", "MACRO_FINAL"
+    )
+
+    cpp_code_out = """\
+#include "MyInclude.h"
+A::A()
+{
+MACRO_INITIAL();
+
+MACRO_FINAL();
+}
+A::~A(){
+
+}
+"""
+    assert string_list_to_string(file_lines_out) == cpp_code_out
+
+def test_method_extra_bracket():
+    cpp_code_in = """\
+A::B()
+{
+  for ( , , ){
+  }
+}"""
+    file_lines_in = string_to_string_list(cpp_code_in)
+    file_lines_out = add_cpp_include_and_macros(
+        file_lines_in, "MyInclude.h", "MACRO_INITIAL", "MACRO_FINAL"
+    )
+
+    cpp_code_out = """\
+#include "MyInclude.h"
+A::B()
+{
+MACRO_INITIAL();
+  for ( , , ){
+  }
+MACRO_FINAL();
+}
+"""
+    assert string_list_to_string(file_lines_out) == cpp_code_out
+    
+def test_method_class_definition():
+    cpp_code_in = """\
+class TeamCityPrinter : public ::testing::EmptyTestEventListener {
+};"""
+    file_lines_in = string_to_string_list(cpp_code_in)
+    file_lines_out = add_cpp_include_and_macros(
+        file_lines_in, "MyInclude.h", "MACRO_INITIAL", "MACRO_FINAL"
+    )
+
+    cpp_code_out = """\
+#include "MyInclude.h"
+class TeamCityPrinter : public ::testing::EmptyTestEventListener {
+};
+"""
+    assert string_list_to_string(file_lines_out) == cpp_code_out
+
+
 
 if __name__ == "__main__":
-    pass
+    rootDir = '.'
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        print('Found directory: %s' % dirName)
+        for file in fileList:
+#        for file in ['SortWireDataOp.cpp']:
+            if file.endswith(".cpp"):
+                full_file_name = os.path.join(dirName, file)
+                #full_file_name = file
+                print('\t%s' % full_file_name)
+                try:
+                    input_file = open(full_file_name)
+                    input_lines = input_file.readlines()
+                    input_file.close()
+                    output_file = open(full_file_name, 'w')
+
+                    output_lines = add_cpp_include_and_macros(
+                        input_lines,'libProfiler.h','PROFILER_START_F','PROFILER_END'
+                    )
+                    output_file.writelines(output_lines)
+                    output_file.close()
+
+                except:
+                    print("Error processing: " + full_file_name)
+
+  
     #test_method_one_line()
